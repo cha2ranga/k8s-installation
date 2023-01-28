7e68d57a65109fa77137a23062f6a96dddabcd5b
Chathuranga Pitigala <cha2ranga@gmail.com>
2023-01-29 07:12:34 +0800

    v23

--- scripts/1_k8s_install_part1.bash
+++ scripts/1_k8s_install_part1.bash
@@ -9,6 +9,11 @@
 ## Install packages
 
 yum install epel-release tree screen wget bash-completion yum-utils vim -y
+
+echo ""
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  Extra repo added !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo ""
+
 yum install device-mapper-persistent-data lvm2  device-mapper-multipath iscsi-initiator-utils git bash-completion nfs-utils -y
 
 #iscsi-initiator-utils-devel
